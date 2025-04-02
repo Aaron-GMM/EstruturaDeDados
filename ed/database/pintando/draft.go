588d9c9341e8be4e8fd83a78e2d0c8e000de6da5
Aaron Gibran <aarongibran13@gmail.com>
2025-04-02 19:15:59 -0300

    Completed: pintando|starting:mumia

--- ed/database/pintando/draft.go
+++ ed/database/pintando/draft.go
@@ -1,5 +1,31 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
 func main() {
-    fmt.Println("qxcode")
+    var ladoA float32  = 0.00
+    var ladoB float32  = 0.00
+    var ladoC float32  = 0.00
+
+    var semiPerimetro float32 = 0.0
+    var raiz float32 = 0.0
+
+    fmt.Scanln(&ladoA)
+    fmt.Scanln(&ladoB)
+    fmt.Scanln(&ladoC)
+
+
+
+    semiPerimetro = (ladoA+ladoB+ladoC)/ 2
+    raiz = semiPerimetro * ((semiPerimetro-ladoA) * (semiPerimetro-ladoB) * (semiPerimetro-ladoC))
+    var area float32 = fraiz(raiz) 
+    
+    fmt.Printf("%.2f\n", area)
+}
+
+func fraiz(raiz float32) float32{
+    raiz = float32(math.Sqrt(float64(raiz)))
+    return raiz
+
 }
