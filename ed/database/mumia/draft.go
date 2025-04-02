5ff876ca7d344b43b753a8c5cb5643a7245bef3e
Aaron Gibran <aarongibran13@gmail.com>
2025-04-02 20:41:41 -0300

    Completed: mumia|starting: fuga

--- ed/database/mumia/draft.go
+++ ed/database/mumia/draft.go
@@ -1,5 +1,22 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var nome string = " "
+    var idade int32 = 0
+    var faixaEtaria string 
+    fmt.Scanln(&nome)
+    fmt.Scanln(&idade)
+    switch {
+	case idade < 12:
+		faixaEtaria = "crianca"
+	case idade < 18:
+		faixaEtaria = "jovem"
+	case idade < 65:
+		faixaEtaria = "adulto"
+    case idade<1000:
+		faixaEtaria = "idoso"
+    default:
+         faixaEtaria = "idoso"
+	}
+    fmt.Println(nome+ " eh "+ faixaEtaria)
 }
