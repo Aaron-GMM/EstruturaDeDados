02540fb3f5f1b3e5bf3a0574aeb341a892cbc0e7
Aaron Gibran <aarongibran13@gmail.com>
2025-04-06 18:55:49 -0300

    Completed: pedra|starting: casis

--- ed/database/pedra/draft.go
+++ ed/database/pedra/draft.go
@@ -1,27 +1,38 @@
 package main
 import "fmt"
 func main() {
-    var numCompeditdor int8
-    //inserir vetores das pedras 
-    // vetores de competidores
+    var numeroCompetidores int8
+	fmt.Scanln(&numeroCompetidores)
 
-    for i:= 0; i<int(numCompeditdor); i++  {
-        fmt.Scan(&disPedraA)
-        fmt.Scan(&disPedraB)
-        if ( i==0) {
-            difCompetidoUm = disPedraA-disPedraB
-        }
-        if(i ==1){
-            difCompetidoDois = disPedraA - disPedraB
-        }
-    }
+	distancias := make([][2] int, numeroCompetidores) 
 
-    if difCompetidoUm<0 {
-         difCompetidoUm  *=(-1)
-    }
-    if difCompetidoDois<0 {
-        difCompetidoDois *=(-1)
-    }
+	for i:= 0; i< int(numeroCompetidores); i++{
+		fmt.Scan(&distancias[i][0],&distancias[i][1])
+	}
 
+	vencedor := -1
+	melhorPont := 91
 
+	for i := 0; i < int(numeroCompetidores); i++ {
+		pedraA:= distancias[i][0]
+		pedraB:= distancias[i][1]
+		if (pedraA <10 || pedraB <10){
+			continue
+		}
+		var diferenca int
+		if pedraA > pedraB{
+		 diferenca = pedraA-pedraB
+		}else{
+			diferenca = pedraB-pedraA
+		}
+		if diferenca < melhorPont {
+			melhorPont = diferenca
+			vencedor = i
+		}
+	}
+	if vencedor != -1 {
+		fmt.Println(vencedor)
+	} else {
+		fmt.Println("sem ganhador")
+	}
 }
