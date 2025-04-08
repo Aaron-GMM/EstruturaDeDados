f65e4b0f53b38b8b1d8f031d59233e1aa4b12e80
Aaron Gibran <aarongibran13@gmail.com>
2025-04-08 17:25:40 -0300

    Completed: casais |starting: baruel

--- ed/database/casais/draft.go
+++ ed/database/casais/draft.go
@@ -1,5 +1,36 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
 func main() {
-    fmt.Println("qxcode")
+    var n int 
+    var qcasais int = 0
+    fmt.Scanln(&n)
+
+	casais := make([] int, n) 
+   
+    for i:= 0; i < n; i++{
+        fmt.Scan(&casais[i])
+    
+    }
+
+    for i := 0; i < n-1; i++ {
+        if casais[i] == 9999 {
+            continue
+        }
+        for j:=i+1; j<n; j++{
+            if  casais[j] == 9999 {
+                continue
+            }
+            if  casais[i]+casais[j] == 0 {
+                qcasais++
+                casais[i] = 9999
+                casais[j] = 9999
+                break
+            }
+        }
+     
+    }
+    fmt.Printf("%d\n", qcasais)
 }
